880d560d94aa515263e09214b41ecad47132679b
Nils Langner <nils.langner@phmlabs.com>
2012-07-12 13:20:40 -0400

    Renamed ValidJson to Valid

---
+++ src/LiveTest/TestCase/General/Json/Valid.php
@@ -0,0 +1,31 @@
+<?php
+namespace LiveTest\TestCase\General\Json;
+
+use LiveTest\TestCase\Exception;
+use Base\Http\Request\Request;
+use Base\Http\Response\Response;
+use LiveTest\TestCase\TestCase;
+
+/**
+ * @author Nicolas Lang
+ */
+class Valid implements TestCase
+{  
+    private $json_errors = array(
+        JSON_ERROR_NONE => 'No Error',
+        JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+        JSON_ERROR_STATE_MISMATCH => 'Underflow or the modes mismatch',
+        JSON_ERROR_CTRL_CHAR => 'Unexpected control character found',
+        JSON_ERROR_SYNTAX => 'Syntax error, malformed JSON',
+        JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+    );
+    
+    public function test (Response $response, Request $request)
+    {
+        $result = json_decode ($response->getBody());
+
+        if ($result === null) {
+            throw new Exception("The given JSON Data is not well formed (last error: " . $this->json_errors[json_last_error()] . ").");
+        }
+    }
+}
