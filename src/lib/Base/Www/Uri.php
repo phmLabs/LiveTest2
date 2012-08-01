e6e774fde59ddab70627a00741a1a238fe9b4ee6
Nils Langner <contact@phmlabs.com>
2012-08-01 11:43:43 +0200

    Added idvalid method again

--- src/lib/Base/Www/Uri.php
+++ src/lib/Base/Www/Uri.php
@@ -31,6 +31,55 @@ class Uri
         return $this->uri;
     }
 
+
+    /**
+     * This static function returns true if a given string represents a valid
+     * uri, otherwise false.
+     *
+     * @param string $uriString
+     */
+    public static function isValid ($uriString)
+    {
+        /**
+         *
+         * @todo : Check if Zend_Validator_Callback can do the same.
+         *       Used from:
+         *       http://phpcentral.com/208-url-validation-in-php.html#post576
+         */
+        $urlregex = "^((https?|ftp)\:\/\/)?";
+
+        // USER AND PASS (optional)
+        $urlregex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?";
+
+        // HOSTNAME OR IP
+        $urlregex .= "[a-z0-9+\$_-]+(\.[a-z0-9+\$_-]+)*"; // http://x = allowed
+        // (ex.
+        // http://localhost,
+        // http://routerlogin)
+        // $urlregex .=
+        // "[a-z0-9+\$_-]+(\.[a-z0-9+\$_-]+)+";
+        // // http://x.x =
+        // minimum
+        // $urlregex .=
+        // "([a-z0-9+\$_-]+\.)*[a-z0-9+\$_-]{2,3}";
+        // // http://x.xx(x) =
+        // minimum
+        // use only one of the
+        // above
+
+        // PORT (optional)
+        $urlregex .= "(\:[0-9]{2,5})?";
+        // PATH (optional)
+        $urlregex .= "(\/([a-z0-9+\$_-~]\.?)+)*\/?";
+        // GET Query (optional)
+        $urlregex .= "(\?[a-z+&\$_.-][a-z0-9;:@\/&%=+\$_.-]*)?";
+        // ANCHOR (optional)
+        $urlregex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?\$";
+
+        $urlregex = "/" . $urlregex . "/";
+        return (bool) preg_match($urlregex, $uriString);
+    }
+
     /**
      * @param unknown_type $uriString
      */
