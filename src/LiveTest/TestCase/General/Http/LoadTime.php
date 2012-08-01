1d629cd201b5521bf7f7da8c01b04a6c905aab12
Nils Langner <contact@phmlabs.com>
2012-08-01 11:39:04 +0200

    Removed isValid method from uri

--- src/LiveTest/TestCase/General/Http/LoadTime.php
+++ src/LiveTest/TestCase/General/Http/LoadTime.php
@@ -1,36 +1,32 @@
 <?php
 
 /*
-* This file is part of the LiveTest package.
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
-
+ * This file is part of the LiveTest package. For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace LiveTest\TestCase\General\Http;
-
 use Base\Http\Request\Request;
-
 use LiveTest\TestCase\Exception;
 use LiveTest\TestCase\TestCase;
-
 use Base\Http\Response\Response;
 
 class LoadTime implements TestCase
 {
-  private $maxLoadTime;
 
-  public function init($maxLoadTime)
-  {
-    $this->maxLoadTime = $maxLoadTime;
-  }
+    private $maxLoadTime;
 
-  public function test(Response $response, Request $request)
-  {
-    $loadTime = $response->getDuration();
-    if ($loadTime > $this->maxLoadTime)
+    public function init ($maxLoadTime)
     {
-      throw new Exception('The time to load the website is too long (current: ' . $loadTime . ', max: ' . $this->maxLoadTime . ')');
+        $this->maxLoadTime = $maxLoadTime;
     }
-  }
+
+    public function test (Response $response, Request $request)
+    {
+        $loadTime = $response->getDuration();
+        if ($loadTime > $this->maxLoadTime) {
+            throw new Exception(
+                    'The time to load the website is too long (current: ' . $loadTime . ', max: ' . $this->maxLoadTime .
+                             ')');
+        }
+    }
 }
--- src/lib/Base/Www/Uri.php
+++ src/lib/Base/Www/Uri.php
@@ -15,38 +15,28 @@ class Uri
         // cannot find any error in it. So: lets validate it by Symfony's
         // Request-Object.
         /*
-         * if (!self::isValid($uriString)) { throw new \Base\Www\Exception('The
-         * given string (' . $uriString . ') does not represent a valid uri'); }
+         * if (!self::isValid($uriString)) { throw new \Base\Www\Exception('The given string (' . $uriString . ') does
+         * not represent a valid uri'); }
          */
         $this->uri = $uriString;
     }
 
-    /**
-     * Enter description here ...
-     */
     public function __toString ()
     {
         return $this->toString();
     }
 
-    /**
-     * Enter description here ...
-     */
     public function toString ()
     {
         return $this->uri;
     }
 
     /**
-     *
-     *
-     * Enter description here ...
-     *
      * @param unknown_type $uriString
      */
     public function concatUri ($uriString)
     {
-        if ((strpos($uriString, 'http://') === false) && (strpos($uriString, 'https://') === false))  {
+        if ((strpos($uriString, 'http://') === false) && (strpos($uriString, 'https://') === false)) {
             if (strpos($uriString, '/') === 0) {
                 $url = $this->uri . substr($uriString, 1);
             } else {
@@ -60,7 +50,6 @@ class Uri
     }
 
     /**
-     *
      * @param unknown_type $uriString
      */
     private function checkCorrectUrl ($uriString)
@@ -77,56 +66,9 @@ class Uri
         return $url;
     }
 
-    /**
-     * This static function returns true if a given string represents a valid
-     * uri, otherwise false.
-     *
-     * @param string $uriString
-     */
-    public static function isValid ($uriString)
-    {
-        /**
-         *
-         * @todo : Check if Zend_Validator_Callback can do the same.
-         *       Used from:
-         *       http://phpcentral.com/208-url-validation-in-php.html#post576
-         */
-        $urlregex = "^((https?|ftp)\:\/\/)?";
-
-        // USER AND PASS (optional)
-        $urlregex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?";
-
-        // HOSTNAME OR IP
-        $urlregex .= "[a-z0-9+\$_-]+(\.[a-z0-9+\$_-]+)*"; // http://x = allowed
-                                                          // (ex.
-                                                          // http://localhost,
-                                                          // http://routerlogin)
-                                                          // $urlregex .=
-                                                          // "[a-z0-9+\$_-]+(\.[a-z0-9+\$_-]+)+";
-                                                          // // http://x.x =
-                                                          // minimum
-                                                          // $urlregex .=
-                                                          // "([a-z0-9+\$_-]+\.)*[a-z0-9+\$_-]{2,3}";
-                                                          // // http://x.xx(x) =
-                                                          // minimum
-                                                          // use only one of the
-                                                          // above
-
-        // PORT (optional)
-        $urlregex .= "(\:[0-9]{2,5})?";
-        // PATH (optional)
-        $urlregex .= "(\/([a-z0-9+\$_-~]\.?)+)*\/?";
-        // GET Query (optional)
-        $urlregex .= "(\?[a-z+&\$_.-][a-z0-9;:@\/&%=+\$_.-]*)?";
-        // ANCHOR (optional)
-        $urlregex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?\$";
-
-        $urlregex = "/" . $urlregex . "/";
-        return (bool) preg_match($urlregex, $uriString);
-    }
-
     public function getDomain ()
     {
+        // @todo look for the third /, not after position 8
         $pos = strpos($this->uri, '/', 8);
         if ($pos !== false) {
             return new self(substr($this->uri, 0, $pos));
