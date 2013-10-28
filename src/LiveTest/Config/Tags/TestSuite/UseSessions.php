331aa7161dbc1e6cb79f30dfe320ecf5ef6b0052
Sebastian Neubert <sene@hrz.tu-chemnitz.de>
2013-10-28 17:43:16 +0100

    removed typo

--- src/LiveTest/Config/Tags/TestSuite/UseSessions.php
+++ src/LiveTest/Config/Tags/TestSuite/UseSessions.php
@@ -10,7 +10,7 @@ class UseSessions extends Base
     protected function doProcess(\LiveTest\Config\TestSuite $config, $sessionNames)
     {
         $testCaseConfig = $config->getCurrentTestCaseConfig();
-        if (is_array($sessionsNames) ) {
+        if (is_array($sessionNames) ) {
             foreach ($sessionNames as $sessionName) {
                 $testCaseConfig->addSession($sessionName);
             }
